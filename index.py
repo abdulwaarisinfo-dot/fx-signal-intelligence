ccc986fa3b3b9675d26bcd5869cb17419fc9ae5b
WARIS ALI <abdulwaarisinfo@gmail.com>
2026-06-22 01:20:16 +0500

    Update index.py

--- index.py
+++ index.py
@@ -256,7 +256,7 @@ def _bool_env(key: str, default: bool = True) -> bool:
     return val in ("1", "true", "yes", "on")
 
 REDDIT_ENABLED   = _bool_env("REDDIT_ENABLED",   True)
-TWITTER_ENABLED  = _bool_env("TWITTER_ENABLED",  False)
+TWITTER_ENABLED  = _bool_env("TWITTER_ENABLED",  True)
 TELEGRAM_ENABLED = _bool_env("TELEGRAM_ENABLED", False)
 
 
